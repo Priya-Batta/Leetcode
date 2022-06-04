7be9c78b6cb4b3b543f1bd14afeb1fb88e01bdd5
Priya-Batta <44232719+Priya-Batta@users.noreply.github.com>
2022-06-04 19:09:42 +0530

    Time: 2 ms, Memory: 42.1 MB - LeetHub

---
+++ sqrtx/sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0)
+            return 0;
+        int start = 1, end = x;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(mid <= x/mid && mid+1 >x/(mid+1))
+                return mid;
+            else if(mid > x/mid)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return start;
+    }
+}

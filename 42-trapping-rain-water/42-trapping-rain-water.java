0321a87d82959f2606dd6849b5a43f8a2f8ac762
Priya-Batta <44232719+Priya-Batta@users.noreply.github.com>
2022-09-18 16:36:11 +0530

    Time: 1 ms (99.76%), Space: 43.1 MB (90.31%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int left = 0; int right = height.length - 1;
+        int maxLeft = 0; int maxRight = 0;
+        
+        int totalWater = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= maxLeft) { 
+                    maxLeft = height[left]; 
+                } else { 
+                    totalWater += maxLeft - height[left]; 
+                }
+                left++;
+            } else {
+                if (height[right] >= maxRight) { 
+                    maxRight = height[right]; 
+                } else {
+                    totalWater += maxRight - height[right]; 
+                }
+                right--;
+            }
+        }
+        return totalWater;
+    }
+}
